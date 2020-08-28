178e1b9e8ddf0767f28591601b67ae34fc582b19
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-28 19:27:33 -0400

    Setup redux with middleware

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    smurfs: [],
+    loadingSmurfs: false,
+    error: null
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
